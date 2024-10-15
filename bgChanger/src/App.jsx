823e1a058bcf274303dad42d3136f792adc371b8
waqar864 <waqarkhan864@gmail.com>
2024-10-15 21:41:47 +0500

    background color changer with state hook

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+function App() {
+   const [Color, setColor] = useState("olive");
+
+  return (
+    
+    <div className='w-full h-screen duration-200'
+    style={{ backgroundColor: Color }}
+    >
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white rounded-xl p-4'>
+          <button onClick={() => setColor("black")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{ backgroundColor: "black" }}>Black</button>
+          <button onClick={() => setColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{ backgroundColor: "red" }}>Red</button>
+          <button onClick={() => setColor("green")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{ backgroundColor: "green" }}>Green</button>
+          <button onClick={() => setColor("purple")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{ backgroundColor: "purple" }}>purple</button>
+          <button onClick={() => setColor("blue")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{ backgroundColor: "blue" }}>Blue</button>
+          <button onClick={() => setColor("olive")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{ backgroundColor: "olive" }}>Olive</button>
+          <button onClick={() => setColor("yellow")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{ backgroundColor: "yellow" }}>Yellow</button>
+          <button onClick={() => setColor("orange")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{ backgroundColor: "orange" }}>Orange</button>
+        </div>
+      </div>
+      </div> 
+    
+  )
+}
+
+export default App
